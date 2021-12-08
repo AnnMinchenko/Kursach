c7c7f3b1c618a1310c8a4b7e2f7b20e925b11189
Анна Минченко <grozka00@mail.ru>
2021-12-08 14:18:55 +0800

    Создан класс частиц; вывод частиц на форму

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,36 @@ namespace Kursach
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
+
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                
+                particles.Add(particle);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                foreach (var particle in particles)
+                {
+                    particle.Draw(g);
+                }
+            }
+
+            picDisplay.Invalidate();
         }
     }
 }
