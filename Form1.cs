6c7e3a7e7de37f52dbd7d5160bd463e6d50cc1f3
Анна Минченко <grozka00@mail.ru>
2021-12-08 14:59:50 +0800

    Добавлено появление частиц возле курсора; изменено начальное место появления частиц

--- Form1.cs
+++ Form1.cs
@@ -21,29 +21,21 @@ namespace Kursach
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
-            for (var i = 0; i < 500; ++i)
-            {
-                var particle = new Particle();
-                
-                particle.X = picDisplay.Image.Width / 2;
-                particle.Y = picDisplay.Image.Height / 2;
-                
-                particles.Add(particle);
-            }
+            
         }
 
         private void UpdateState()
         {
             foreach (var particle in particles)
             {
-                particle.Life -= 1; 
-                                    
+                particle.Life -= 1;
+
                 if (particle.Life < 0)
                 {
                     particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
 
-                    particle.X = picDisplay.Image.Width / 2;
-                    particle.Y = picDisplay.Image.Height / 2;
                     particle.Direction = Particle.rand.Next(360);
                     particle.Speed = 1 + Particle.rand.Next(10);
                     particle.Radius = 2 + Particle.rand.Next(10);
@@ -53,6 +45,21 @@ namespace Kursach
                     var directionInRadians = particle.Direction / 180 * Math.PI;
                     particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
                     particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    var particle = new Particle();
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
                 }
             }
         }
@@ -76,6 +83,15 @@ namespace Kursach
             }
 
             picDisplay.Invalidate();
+        }
+
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
         }
     }
 }
